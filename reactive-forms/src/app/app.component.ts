745326040b817c9944c2b63eb658fa87bbd3fcbe
sundaramhpe <sunram_c@hotmail.com>
2020-03-02 08:19:06 +0530

    Form Builder Added

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { FormGroup, FormControl } from '@angular/forms';
+import {FormBuilder}from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  constructor(private fb:FormBuilder){}
+
+  registrationFrom = this.fb.group({
+    userName : [''],
+    password : [''],
+    confirmPassword : ['']
+  })
+ /* registrationFrom = new FormGroup({
+    userName : new FormControl('Sundaram C'),
+    password : new FormControl(''),
+    confirmPassword : new FormControl('')
+  }); */
+  loadApiData(){
+    this.registrationFrom.patchValue({
+      userName:'Sundaram C',
+      password:'test',
+      confirmPassword:'test'
+
+    });
+
+  }
 }
