acae51213d5101c42904d6daad220dccdaa23b0a
sundaramhpe <sunram_c@hotmail.com>
2020-03-02 06:40:02 +0530

    initiate the node server

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {User} from './user'
 import { from } from 'rxjs';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,15 @@ import { from } from 'rxjs';
 export class AppComponent {
   topics =['Angular','React','Vue'];
   userModel = new User("","sun@com",555555,"Angular")
+  constructor(private _enrollmentService:EnrollmentService){}
 
   onSubmit(){
-    console.log(this.userModel);
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log("Sucess",data),
+      error => console.error('Error!',error)
+      
+    )
+   
   }
 }
