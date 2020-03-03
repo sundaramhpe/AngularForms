117ad5660f98e707cd94593637fcfb46357de1b8
sundaramhpe <sunram_c@hotmail.com>
2020-03-03 20:22:30 +0530

    installed mongoose server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const PORT=3000;
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors);
